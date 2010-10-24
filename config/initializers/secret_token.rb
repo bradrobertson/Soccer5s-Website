55d605334593f98061ac81764b13da4a7092a510
Brad Robertson <bradleyrobertson@gmail.com>
2010-10-24 16:43:45 -0400

    initial app, will create template from this

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soccer5s::Application.config.secret_token = 'e5b71849e826b00663417dffb39e301e92f94a1d41fea711e3ec7aa3d1223449929352d849696ac7cce9da8a0815f17271aa26dcf9c4bfc26a73d8a1e5b84ad1'
