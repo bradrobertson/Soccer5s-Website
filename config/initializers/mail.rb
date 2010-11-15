ff1fd31ca44691248f39d6ddc51e0e73287a40d9
Brad Robertson <bradleyrobertson@gmail.com>
2010-11-14 19:36:59 -0500

    using app user for mail sending

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => 'soccer5s.com',
-  :user_name            => 'info@soccer5s.com',
-  :password             => 'mariners',
+  :user_name            => 'app@soccer5s.com',
+  :password             => 'phei2ahB',
   :authentication       => 'plain',
   :enable_starttls_auto => true
 }
