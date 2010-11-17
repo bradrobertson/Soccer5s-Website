b5f5e228d4ab549ad2d08983c240e5ace8397177
Brad Robertson <bradleyrobertson@gmail.com>
2010-11-16 21:43:09 -0500

    adding staging branch and cufon for fonts

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,11 +4,9 @@ set :rvm_ruby_string, 'ruby-1.9.2-p0'
 set :rvm_type, :user
 
 set :application, "soccer5s"
-set :deploy_to, "/var/www/#{application}"
 
 set :scm, :git
 set :repository,  "/Users/bradrobertson/git/soccer5s"
-set :branch, "master"
 set :deploy_via, :copy
 
 set :copy_exclude, [".git", ".gitignore"]     # ignore all git files
@@ -18,10 +16,20 @@ set :user, 'app'
 
 server "soccer5s.com", :app, :web, :db, :primary => true
 
+task :staging do
+  set :deploy_to, "/var/www/#{application}-staging"
+  set :branch, "staging"
+end
+
+task :production do
+  set :deploy_to, "/var/www/#{application}"
+  set :branch, "master"
+end
+
 namespace :deploy do
   task :start, :roles => :app do
-      run "touch #{current_release}/tmp/restart.txt"
-    end
+    run "touch #{current_release}/tmp/restart.txt"
+  end
     
   task :stop do ; end
   
@@ -29,13 +37,5 @@ namespace :deploy do
     run "touch #{current_release}/tmp/restart.txt"
   end
   
-  after "deploy:symlink", "db:link"
   before "deploy:restart", "deploy:migrate"
-end
-
-namespace :db do
-  desc "Link sqlite db to shared file"
-  task :link, :roles => :db do
-    run "ln -s #{File.join(shared_path,'db','production.sqlite3')} #{File.join(current_path,'db')}"
-  end
 end
