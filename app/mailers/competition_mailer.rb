249f5d85ac9bba8bca75249d97094b98895d1537
Brad Robertson <bradleyrobertson@gmail.com>
2011-06-29 08:20:08 -0400

    added mailer for competition

---
+++ app/mailers/competition_mailer.rb
@@ -0,0 +1,14 @@
+class CompetitionMailer < ActionMailer::Base
+  default :from => "Soccer5s <info@soccer5s.com>"
+  
+  def client_email(client)
+    mail :to => client.email, :subject => "Your Request Was Received"
+  end
+  
+  def admin_email(competition)
+    @competition = competition
+    address = Rails.env.production? ? 'info@soccer5s.com' : ['bradleyrobertson@gmail.com', 'jtsmith@soccer5s.com']
+    mail :to => address, :subject => "Mariner Competition submitted"
+  end
+  
+end
--- app/models/competition.rb
+++ app/models/competition.rb
@@ -3,6 +3,14 @@ class Competition < ActiveRecord::Base
   validates :name, :presence => true
   validates :email, :presence => true, 
     :length => {:minimum => 3, :maximum => 254},
-    :uniqueness => true,
     :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
+    
+  after_create :send_mail
+  
+  private
+  
+  def send_mail
+    CompetitionMailer.client_email(self).deliver
+    CompetitionMailer.admin_email(self).deliver
+  end
 end
