1933e3dc0dc9a4fa2e6841694a063cf2046815c1
Brad Robertson <bradleyrobertson@gmail.com>
2011-07-01 09:08:20 -0400

    rails is a bitch, couldnt get validates_acceptance_of to work

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -4,10 +4,13 @@ class Competition < ActiveRecord::Base
   validates :email, :presence => true, 
     :length => {:minimum => 3, :maximum => 254},
     :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
-  validates_acceptance_of :terms
+  # validates_acceptance_of :terms, :accept => [true, "1", "true"]
   validates_presence_of :team_name
     
   after_create :send_mail
+  
+  attr_accessible :name, :email, :terms, :team_name, :favorite_mariner, :telephone, :comments
+  attr_accessor :terms_and_conditions
   
   private
   
