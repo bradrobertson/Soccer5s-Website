79b2996ef8070ffb88b2f84ef15efb1709d156af
Brad Robertson <bradleyrobertson@gmail.com>
2012-05-01 23:23:46 -0400

    fix precompile

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Soccer5s
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # requires so heroku doesn't connect to db on precompile
+    config.assets.initialize_on_precompile = false
   end
 end
