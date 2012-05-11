4650ff678a6422199fab4aa58cb7684ec6f76dd1
Brad Robertson <bradleyrobertson@gmail.com>
2012-05-11 08:09:00 -0400

    caching static assets on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,7 @@ Soccer5s::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.static_cache_control = "public, max-age=2592000"
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
@@ -28,7 +29,7 @@ Soccer5s::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
