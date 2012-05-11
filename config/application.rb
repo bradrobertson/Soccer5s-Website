e2d3ae41f94b65f44fcd38b0f25073adde2bea60
Brad Robertson <bradleyrobertson@gmail.com>
2012-05-11 08:09:50 -0400

    expire assets

--- config/application.rb
+++ config/application.rb
@@ -63,7 +63,7 @@ module Soccer5s
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
     
     # requires so heroku doesn't connect to db on precompile
     config.assets.initialize_on_precompile = false
