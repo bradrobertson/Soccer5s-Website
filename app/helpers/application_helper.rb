dbb9b80870e2c792200d52ef064caf76a54082c7
Brad Robertson <bradleyrobertson@gmail.com>
2012-09-17 08:12:28 -0400

    fix styling for longer sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,26 @@
 module ApplicationHelper
-  
+
   def section
     controller_name.underscore
   end
-  
+
   def page
     action_name.underscore
+  end
+
+  def sidebar
+    content_tag :div, id: 'sidebar' do
+      content_tag :div, class: 'wrap' do
+        content_for :sidebar
+      end
+    end if content_for? :sidebar
+  end
+
+  def sidebar_class
+    content_for?(:sidebar) ? 'with-sidebar' : ''
+  end
+
+  def main_content(&block)
+    content_tag(:div, id: 'main', class: sidebar_class, &block)
   end
 end
