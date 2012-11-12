f86ce4ea926c4486ce39b67a98f63e992e865f41
Brad Robertson <bradleyrobertson@gmail.com>
2012-11-11 20:08:40 -0500

    generated refinery content

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Soccer5s::Application.routes.draw do
 
+  # This line mounts Refinery's routes at the root of your application.
+  # This means, any requests to the root URL of your application will go to Refinery::PagesController#home.
+  # If you would like to change where this extension is mounted, simply change the :at option to something different.
+  #
+  # We ask that you don't use the :as option here, as Refinery relies on it being the default of "refinery"
+  mount Refinery::Core::Engine, :at => '/'
+
+
   resources :clients, :only => [:new, :index, :show, :create]
 
   root :to => "public#home"
