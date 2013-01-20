5d9427ef06f28ef5a949faad0d8bbd3ee2c54838
Brad Robertson <bradleyrobertson@gmail.com>
2013-01-20 15:45:08 -0500

    fixing contact tabs for now

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -6,6 +6,11 @@ $(function(){
 	$(".scrollable").scrollable({circular:true}).navigator().autoscroll(8000);
   $(".fancybox").fancybox();
 
+  // HACK
+  if ($("#new.inquiries, #thank_you.inquiries").length) {
+    $("ul.tabs").tabs("div.panes > div");
+  }
+
 	// welcome our user
 	new app.Owner().welcome();
 	// Set access time on visited
