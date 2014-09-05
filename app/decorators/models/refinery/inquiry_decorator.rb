e20cf069403b518d3b08fa99135a23f1922f63ba
Brad Robertson <dev+brad@influitive.com>
2014-09-04 22:49:01 -0400

    spam bot protection

--- app/decorators/models/refinery/inquiry_decorator.rb
+++ app/decorators/models/refinery/inquiry_decorator.rb
@@ -1,3 +1,10 @@
 Refinery::Inquiries::Inquiry.instance_eval do
   validates :phone, presence: true
+  attr_accessible :bot_protection
+  attr_accessor :bot_protection
+
+  validate do
+    errors.add(:base, "sorry friend") if bot_protection.present?
+    false
+  end
 end
